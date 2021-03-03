d0683b915ec03a331ee98c7904debf6fd30b925b
Marios Mertzanidis <58370658+MariosMertzanidis@users.noreply.github.com>
2021-03-03 20:46:13 +0200

    Delete DHT-Chord directory

--- DHT-Chord/checkRange.py
+++
@@ -1,7 +0,0 @@
-import myInfo as inf
-
-def inMyRange(num, myRange):
-    if inf.myHash <= myRange :
-        return (num<=inf.myHash) and (num>=myRange)
-    else:
-        return (num<=inf.myHash) or (num>=myRange)
