757a0174901c9ca480796974b992000cdda4c7da
Marios Mertzanidis <58370658+MariosMertzanidis@users.noreply.github.com>
2021-03-13 21:48:09 +0200

    Delete checkRange.py

--- checkRange.py
+++
@@ -1,7 +0,0 @@
-import myInfo as inf
-
-def inMyRange(num, myRange):
-    if inf.myHash >= myRange :
-        return (int(num)<=inf.myHash) and (int(num)>=myRange)
-    else:
-        return (int(num)<=inf.myHash) or (int(num)>=myRange)
